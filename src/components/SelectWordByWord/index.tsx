32d3a47192c54e6a3d5e4e2f4654b38f8b8c6fbe
Tom Hughes <tomhughes1987@gmail.com>
2022-08-05 00:14:50 +0200

    fields start with values

--- src/components/SelectWordByWord/index.tsx
+++ src/components/SelectWordByWord/index.tsx
@@ -8,14 +8,14 @@ export default function SelectWordByWord():JSX.Element {
 
 
   // MID WORD
-  let [middleWord,setMiddleWord]= useState("")
+  let [middleWord,setMiddleWord]= useState(midWord[Math.floor(Math.random()*midWord.length)])
   function getRandMid(e:any):void {
       e.preventDefault();
       setMiddleWord (midWord[Math.floor(Math.random()*midWord.length)]  )
   }
 
   // MAIN WORD
-  let [theMainWord,setTheMainWord]= useState("")
+  let [theMainWord,setTheMainWord]= useState(mainWord[Math.floor(Math.random()*mainWord.length)])
   function getRandMain(e:any):void {
       e.preventDefault();
       setTheMainWord (mainWord[Math.floor(Math.random()*mainWord.length)]  )
@@ -27,7 +27,7 @@ export default function SelectWordByWord():JSX.Element {
     <form >
       {/* FIRST WORD */}
       {/* <label for="Prefixes">Prefixes</label> */}
-      <select id="Prefixes" name="Prefixes" className='firstPart'>
+      <select id="Prefixes" name="Prefixes" className='firstPart' defaultValue={"The"}>
           <option value=""></option>
           <option value="The">The</option>
           <option value="HMS">HMS</option>
