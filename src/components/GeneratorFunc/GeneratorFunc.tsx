d7613c1ae4d19b1aa896da003ab4d32e9725f1b4
Tom Hughes <tomhughes1987@gmail.com>
2022-08-04 11:33:48 +0200

    add component random generator

---
+++ src/components/GeneratorFunc/GeneratorFunc.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { startWord,mainWord,midWord } from '../../data/words'
+import './GeneratorFunc.css'
+
+export default function GeneratorFunc():JSX.Element {
+
+    // for (let i = 0; i < 100; i++) {
+    //     const randomStartWord = startWord[Math.floor(Math.random()*startWord.length)]
+    //     const randomMainWord = mainWord[Math.floor(Math.random()*mainWord.length)]
+    //     const randomMidWord = midWord[Math.floor(Math.random()*midWord.length)]      
+    //     console.log(i, randomStartWord, randomMidWord,randomMainWord)
+    //     // const element = array[i];
+    //   }
+      
+      // let randomStartWord = startWord[Math.floor(Math.random()*startWord.length)]
+      // let randomMainWord = mainWord[Math.floor(Math.random()*mainWord.length)]
+      // let randomMidWord = midWord[Math.floor(Math.random()*midWord.length)]
+
+      // let CompletelyRandom = `${randomStartWord} ${randomMidWord} ${randomMainWord}`
+
+      const [randomStartWord,setRandomStartWord] = useState(startWord[Math.floor(Math.random()*startWord.length)])
+      const [randomMidWord,setRandomMidWord] = useState(midWord[Math.floor(Math.random()*midWord.length)])
+      const [randomMainWord,setRandomMainWord] = useState(mainWord[Math.floor(Math.random()*mainWord.length)])
+
+      const [completelyRandom,setCompletelyRandom] = useState( `${randomStartWord} ${randomMidWord} ${randomMainWord}`)
+
+
+      function getRandFull(e:any):void {
+        e.preventDefault();
+        console.log('btns pressed')
+
+        setRandomStartWord(startWord[Math.floor(Math.random()*startWord.length)]);
+        setRandomMidWord(midWord[Math.floor(Math.random()*midWord.length)]);
+        setRandomMainWord(mainWord[Math.floor(Math.random()*mainWord.length)]);
+
+        setCompletelyRandom(`${randomStartWord} ${randomMidWord}  ${randomMainWord}`)
+
+        // randomStartWord = startWord[Math.floor(Math.random()*startWord.length)]
+        // randomMainWord = mainWord[Math.floor(Math.random()*mainWord.length)]
+        // randomMidWord = midWord[Math.floor(Math.random()*midWord.length)]
+  
+        // CompletelyRandom = `${randomStartWord} ${randomMidWord} ${randomMainWord}`
+        // CompletelyRandom   (mainWord[Math.floor(Math.random()*mainWord.length)]  )
+    }
+
+
+
+
+  return (
+    <>
+      <div id='bottomBox'>
+        <p id='CompletelyRandomName'>{completelyRandom}</p>
+        <button onClick={getRandFull}>Completely Random</button>
+      </div>
+    </>
+  )
+}
