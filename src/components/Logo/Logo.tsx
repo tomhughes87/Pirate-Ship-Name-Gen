ea5e8450cb88c2901c68c102d56654ad993338c4
Tom Hughes <tomhughes1987@gmail.com>
2022-08-07 00:44:48 +0200

    styling adn dice icon

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,22 +5,8 @@ import logo1 from './logo1.png';
 import logo2 from './logo2.png';
 import './Logo.css';
 
-
-
-
 export default function Logo1() {
-    // const logo1 = require ('../../imageslogo1.png')
-
     return (
-    <img id="logo" src={logo1} alt="Logo" />
-    // <img id="logo" src={logo2} alt="Logo" />
-    // <img src={String(logo1)} />
-    // <img  src={String(logo1)} />
-    // <div> hello <div/>
-    // <img src={logo1} className="App-logo" alt="logo" />
-    // <img src={require('../../images./logo1.png')} />
-
-   
-  
+    <img id="logo" src={logo1} alt="Logo" /> 
     )
 }
