1a2546a94e2137c0fe8b0ebe4e749b96352390f5
Tom Hughes <tomhughes1987@gmail.com>
2022-10-24 11:20:21 +0200

    turn off console logs

--- src/data/Sort.tsx
+++ src/data/Sort.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { startWord, midWord, mainWord } from "./words"
 
 export default function SortList() {
-    console.log(startWord.sort())
-    console.log(midWord.sort())
-    console.log(mainWord.sort())
+    // console.log(startWord.sort())
+    // console.log(midWord.sort())
+    // console.log(mainWord.sort())
 
         
     // get the amount of words in each word set
@@ -12,9 +12,9 @@ export default function SortList() {
     let amtOfMidWords = midWord.length
     let amtOfMainWords = mainWord.length
     
-    console.log("amtOfstatingWords",amtOfstatingWords)
-    console.log("amtOfMidWords",amtOfMidWords)
-    console.log("amtOfMainWords",amtOfMainWords)
+    // console.log("amtOfstatingWords",amtOfstatingWords)
+    // console.log("amtOfMidWords",amtOfMidWords)
+    // console.log("amtOfMainWords",amtOfMainWords)
 
     // valid combos:
     let combo1 = amtOfMainWords //eg: englishman
@@ -22,7 +22,7 @@ export default function SortList() {
     let combo3 = amtOfMidWords * amtOfMainWords //eg: laughing englishman
     let combo4 = amtOfstatingWords * amtOfMidWords * amtOfMainWords //eg: the laughing englishman
 
-    console.log("total unique ship names:",combo1+combo2+combo3+combo4)
+    // console.log("total unique ship names:",combo1+combo2+combo3+combo4)
 
 
     return (
