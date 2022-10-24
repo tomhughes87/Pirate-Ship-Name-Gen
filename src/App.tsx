193d33a59329a6f9149a6457df208cce430d7cb4
Tom Hughes <tomhughes1987@gmail.com>
2022-10-24 11:15:23 +0200

    add imports and new divs to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,51 @@
-import React from 'react';
+import React from "react";
 // import logo1 from './logo.svg';
-import './App.css';
-import GeneratorFunc from './components/GeneratorFunc/GeneratorFunc';
-import ShipName from './components/ShipName/ShipName';
-import Logo from './components/Logo/Logo';
-import Helper from './components/Help';
-import Tip from './components/Tip';
-import SelectWordByWord from './components/SelectWordByWord';
+import "./App.css";
+import GeneratorFunc from "./components/GeneratorFunc/GeneratorFunc";
+import ShipName from "./components/ShipName/ShipName";
+import Logo from "./components/Logo/Logo";
+import Helper from "./components/Help";
+import Tip from "./components/Tip";
+import SelectWordByWord from "./components/SelectWordByWord";
 
-import { midWord,mainWord } from './data/words';
-import SortList from './data/Sort';
+import { midWord, mainWord } from "./data/words";
+import SortList from "./data/Sort";
+import FadingAlert from "./components/FadingAlert";
 
 function App() {
-<style>
-@import url('https://fonts.googleapis.com/css2?family=IM+Fell+English+SC&display=swap');
-</style>
-console.log("Amount of midWords:",midWord.length);
-console.log("Amount of mainWords:",mainWord.length);
+  <style>
+    @import
+    url('https://fonts.googleapis.com/css2?family=IM+Fell+English+SC&display=swap');
+  </style>;
+  // console.log("Amount of midWords:",midWord.length);
+  // console.log("Amount of mainWords:",mainWord.length);
 
   return (
-
-    
     <div className="App">
+      {/* <FadingAlert copiedText="This thing" /> */}
 
       <header className="App-header">
-      
-      <SortList />
-      
-      <div id="fakeModal">
-      
-        <Logo />    
-        <ShipName />
-        <Helper />
-    
-        <br/>
-        
-        <div id='dashedBox'>
-          <SelectWordByWord />          
-        </div>
+        <SortList />
 
-        <p id="Method"> ~ OR ~ </p>  
-    
-        <div id='dashedBoxLower'>
-      
-          <GeneratorFunc />
-          
+        <div id="fakeModal">
+          <Logo />
+          <ShipName />
+          <Helper />
+
+          <br />
+
+          <div id="dashedBox">
+            <SelectWordByWord />
+          </div>
+
+          <p id="Method"> ~ OR ~ </p>
+
+          <div id="dashedBoxLower">
+            <GeneratorFunc />
+          </div>
+
+          <Tip />
         </div>
-      
-      <Tip />
-      </div>
       </header>
     </div>
   );
